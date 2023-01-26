81b32dcc327890b7e8dfa55fa8c9b0785b673816
Eduardo Quaresimin Santos <eduardoquaresimin@gmail.com>
2023-01-26 14:25:50 -0300

    Finish extra exercises

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -6,15 +6,59 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.visit('./src/index.html')
     })
 
-    it('verifica o título da aplicação', () => {
+    it('Verifica o título da aplicação', () => {
         cy.title().should('contains', 'Central de Atendimento ao Cliente TAT')  
     })
-    it('preenche os campos obrigatórios e envia o formulário', () => {
+
+    it('Preenche os campos obrigatórios e envia o formulário', () => {
+        cy.get('#firstName').type('Eduardo')
+        cy.get('#lastName').type('Quaresimin Santos')
+        cy.get('#email').type('eduardoquaresimin@gmail.com')
+        cy.get('#open-text-area').type('Escrevendo qualquer coisa.', {delay: 0})
+        cy.contains('button', 'Enviar').click()
+        cy.contains('span', 'Mensagem enviada com sucesso.').should('be.visible')
+    })
+
+    it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+        cy.get('#firstName').type('Eduardo')
+        cy.get('#lastName').type('Quaresimin Santos')
+        cy.get('#email').type('eduardoquaresimin.gmail.com')
+        cy.get('#open-text-area').type('Escrevendo qualquer coisa.')
+        cy.contains('button', 'Enviar').click()
+        cy.contains('span', 'Valide os campos obrigatórios!').should('be.visible')
+    })
+
+    it('Verifica que o campo Telefone aceita apenas números', () => {
+        cy.get('#phone')
+            .type('wasd!@#$%')
+            .should('not.have.text')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
         cy.get('#firstName').type('Eduardo')
         cy.get('#lastName').type('Quaresimin Santos')
         cy.get('#email').type('eduardoquaresimin@gmail.com')
         cy.get('#open-text-area').type('Escrevendo qualquer coisa.')
+        cy.get('#phone-checkbox').check()
         cy.contains('button', 'Enviar').click()
+        cy.contains('span', 'Valide os campos obrigatórios!').should('be.visible')
+    })
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+        cy.get('#firstName').type('Eduardo').should('have.value', 'Eduardo')
+            .clear().should('have.value', '')
+        cy.get('#lastName').type('Quaresimin Santos').should('have.value', 'Quaresimin Santos')
+            .clear().should('have.value', '')
+        cy.get('#email').type('eduardoquaresimin@gmail.com').should('have.value', 'eduardoquaresimin@gmail.com')
+            .clear().should('have.value', '')
+        cy.get('#phone').type('47999112310').should('have.value', '47999112310')
+            .clear().should('have.value', '')
+    })
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+        cy.contains('button', 'Enviar').click()
+        cy.contains('span', 'Valide os campos obrigatórios!').should('be.visible')
+    })
+    it('envia o formuário com sucesso usando um comando customizado', () => {
+        cy.fillMandatoryFieldsAndSubmit()
         cy.contains('span', 'Mensagem enviada com sucesso.').should('be.visible')
     })
   })
